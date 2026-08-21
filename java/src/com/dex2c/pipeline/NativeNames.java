e3344b5616c59a4599db147313b06e78120ebb15
0Dex <163450531+MSR-Cracker@users.noreply.github.com>
2026-08-21 16:41:34 +0300

    baseline: add java/src/com/dex2c/pipeline/NativeNames.java

---
+++ java/src/com/dex2c/pipeline/NativeNames.java
@@ -0,0 +1,9 @@
+package com.dex2c.pipeline;
+
+final class NativeNames {
+    static final String MODULE = "msr";
+    static final String LIB_FILE = "lib" + MODULE + ".so";
+
+    private NativeNames() {
+    }
+}
